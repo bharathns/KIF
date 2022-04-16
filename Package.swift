71460970a0b1017fc0ca10595ef21c23719de356
Bharath NS <bharath.ns99@gmail.com>
2022-04-15 19:13:27 -0500

    add swift package changes

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "KIF",
+    defaultLocalization: "en",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         .library(
@@ -20,19 +21,30 @@ let package = Package(
             dependencies: [],
             publicHeadersPath: "include",
             cSettings: [
+                .headerSearchPath("./**"),
                 .headerSearchPath("ApplePrivateAPIs/"),
                 .headerSearchPath("Additions/"),
                 .headerSearchPath("Classes/"),
                 .headerSearchPath("Visualizer/"),
                 .headerSearchPath("IdentifierTests/"),
             ],
-            linkerSettings: [.linkedFramework("IOKit")]
+            linkerSettings: [ 
+                .linkedFramework("Foundation"),
+                .linkedFramework("IOKit"), 
+                .linkedFramework("UIKit", .when(platforms: [.iOS])),
+                .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
+                .linkedFramework("WebKit", .when(platforms: [.iOS])),
+                .linkedFramework("CoreGraphics", .when(platforms: [.iOS])),
+                .linkedFramework("XCTest", .when(platforms: [.iOS]))
+            ]
         ),
         .testTarget(
             name: "KIFTests",
             dependencies: ["KIF"],
             path: "./KIF Tests",
-            cSettings: [.headerSearchPath("../Sources/KIF/")] // allow to look a "private" headers
+            cSettings: [
+                .headerSearchPath("../Sources/KIF/")
+            ] // allow to look a "private" headers
         ),
     ]
 )
